e526803dc0dbfb66687d45782fdc47c654346d15
Akhtar Khan <akhtar.khan@daffodilsw.com>
2018-06-01 18:48:42 +0530

    setLabel method updated for BarcodeGenerator::Gs1128 to accept blank label

--- CodeItNow/BarcodeBundle/Generator/CINgs1128.php
+++ CodeItNow/BarcodeBundle/Generator/CINgs1128.php
@@ -33,7 +33,8 @@ class CINgs1128 extends CINcode128 {
     private $identifiersId = array();
     private $identifiersContent = array();
     private $identifiersAi = array();
-
+    private $customLabelText=false;
+    
     /**
      * Constructors
      *
@@ -236,13 +237,21 @@ class CINgs1128 extends CINcode128 {
         return $this->noLengthLimit;
     }
 
+    public function setLabel($label){
+        $this->customLabelText = $label;
+    }
+
     /**
      * Parses Text.
      *
      * @param string $text
      */
     public function parse($text) {
-        parent::parse($this->parseGs1128($text));
+        $parsedText = $this->parseGs1128($text);
+        if($this->customLabelText!==false and CINBarcode1D::AUTO_LABEL!=$this->customLabelText){
+            parent::setLabel($this->customLabelText);    
+        }
+        parent::parse($parsedText);
     }
 
     /**
